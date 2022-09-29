c6a8e46b0090789729edbfd133372dde160e4d7d
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-09-29 18:31:55 -0300

    termino do exercicio 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findAnimal = data.species.find(({ name }) => name.includes(animal));
+  // console.log(findAnimal);
+  return findAnimal.residents.every(({ age: idade }) => idade >= age);
 }
+
+console.log(getAnimalsOlderThan('tigers', 17));
 
 module.exports = getAnimalsOlderThan;
