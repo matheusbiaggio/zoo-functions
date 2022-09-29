b9b08c0670dddfc5df1acc4ece85ba99f5358e60
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-09-29 16:41:02 -0300

    termino do primeiro axercicio

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  console.log(ids);
+  if (ids.length === 0) {
+    return [];
+  }
+  return data.species.filter(({ id }) => (ids.includes(id)));
 }
 
 module.exports = getSpeciesByIds;
