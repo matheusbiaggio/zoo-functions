b42408d898a2c86695c7519f7d8d85d7a5af73eb
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-10-01 14:06:33 -0300

    exercicio 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui.
+  let allEntrants = {};
+  const entratsChild = entrants.filter(({ age: ageFilter }) => ageFilter < 18);
+  const entratsAdult = entrants.filter(({ age: ageFilter }) => ageFilter >= 18 && ageFilter < 50);
+  const entratsSenior = entrants.filter(({ age: ageFilter }) => ageFilter >= 50);
+  allEntrants = {
+    child: entratsChild.length,
+    adult: entratsAdult.length,
+    senior: entratsSenior.length,
+  };
+  return allEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  const priceTotalChild = (countEntrants(entrants).child) * (data.prices.child);
+  const priceTotalAdult = (countEntrants(entrants).adult) * (data.prices.adult);
+  const priceTotalSenior = (countEntrants(entrants).senior) * (data.prices.senior);
+  return priceTotalChild + priceTotalAdult + priceTotalSenior;
 }
+
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
